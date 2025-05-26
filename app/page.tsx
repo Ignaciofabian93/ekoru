d05d8ccce1a1bdc46fb76dd4199d339c8f525bf1
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-05-26 12:57:52 -0400

    comming soon

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 
 export default function App() {
   return (
-    <main className="w-full h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl mb-6">Próximamente</h1>
-      <div className="w-[60%]">
-        <Image src="/images/logo.png" alt="Logo Ekoru" width={4096} height={996} />
-      </div>
+    <main className="w-full h-screen flex flex-col items-center justify-center bg-black">
+      <Image
+        src="/images/PROXIMAMENTE.jpg"
+        alt="Logo Ekoru"
+        width={4096}
+        height={996}
+        className="w-full h-full object-contain"
+      />
     </main>
   );
 }
